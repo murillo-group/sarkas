eab731081bfa3c121cef09e4c8a7bf33e6516087
Yongjun Choi <choiyj@msu.edu>
2019-07-03 17:17:30 -0400

    beautifying...

--- S_checkpoint.py
+++ S_checkpoint.py
@@ -9,15 +9,16 @@ from inspect import currentframe, getframeinfo
 import time
 import pickle
 
+
 class checkpoint:
     def __init__(self, params):
         self.params = params
         self.checkpoint_dir = "Checkpoint/"
         if not (os.path.exists(self.checkpoint_dir)):
             os.mkdir(self.checkpoint_dir)
-        
+
         pickle.dump(self.params, open(self.checkpoint_dir+"S_parameters.pickle", "wb"))
 
     def dump(self, pos, vel, acc, it):
         file_name = self.checkpoint_dir+"S_checkpoint_"+str(it)
-        np.savez(file_name, pos = pos, vel = vel, acc = acc)
+        np.savez(file_name, pos=pos, vel=vel, acc=acc)
