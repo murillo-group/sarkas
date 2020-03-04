Fixed cgs units Yukawa

src/S_integrator.py
@@ -454,7 +454,7 @@ def calc_pot_acc(ptcls,params):
     if not (params.Potential.type == "LJ"):
         # Mie Energy of charged systems
         dipole = calc_dipole(ptcls.pos,ptcls.charge)
-        U_MIE = 2.0*np.pi*(dipole[0]**2 + dipole[1]**2 + dipole[2]**2)/(3.0*params.box_volume)
+        U_MIE = 2.0*np.pi*(dipole[0]**2 + dipole[1]**2 + dipole[2]**2)/(3.0*params.box_volume*params.fourpie0)
 
         U = U + U_MIE
 

src/S_params.py
@@ -708,25 +708,25 @@ class Params:
             self.c0 = const.physical_constants["speed of light in vacuum"][0]
             if not (self.Potential.type=="LJ"):
                 # Coulomb to statCoulomb conversion factor. See https://en.wikipedia.org/wiki/Statcoulomb
-                C2statC = 1.0/(10.0*self.c0)
+                C2statC = 10.0*self.c0
                 self.hbar = self.J2erg*const.hbar
                 self.hbar2 = self.hbar**2
                 self.qe = C2statC*const.physical_constants["elementary charge"][0]
                 self.me = 1.0e3*const.physical_constants["electron mass"][0] # grams   
                 self.eps0 = 1.0
+                self.fourpie0 = 1.0
 
         elif (self.Control.units == "mks"):
             self.kB = const.Boltzmann
             self.eV2K = const.physical_constants["electron volt-kelvin relationship"][0]
             self.c0 = const.physical_constants["speed of light in vacuum"][0]
             if not (self.Potential.type=="LJ"):
-                # Coulomb to statCoulomb conversion factor. See https://en.wikipedia.org/wiki/Statcoulomb
                 self.hbar = const.hbar
                 self.hbar2 = self.hbar**2
                 self.qe = const.physical_constants["elementary charge"][0]
                 self.me = const.physical_constants["electron mass"][0]
                 self.eps0 = const.epsilon_0
-
+                self.fourpie0 = 4.0*np.pi*self.eps0
         # Charge systems' parameters.
         self.QFactor = 0.0
         self.tot_net_charge = 0.0
@@ -757,13 +757,7 @@ class Params:
         if not (self.Potential.type == "LJ"):
             for ic in range(self.num_species):
 
-                if (self.Control.units == "cgs"):
-                    C2statC = 1.0/(10.0*const.physical_constants["speed of light in vacuum"][0])
-                    qe = C2statC*const.physical_constants["elementary charge"][0]
-                elif (self.Control.units == "mks"):
-                    qe = const.physical_constants["elementary charge"][0]
-
-                self.species[ic].charge = qe
+                self.species[ic].charge = self.qe
                 
 
                 if hasattr(self.species[ic], "Z"):

src/S_pot_QSP.py
@@ -23,7 +23,7 @@ import math as mt
 import sys
 import scipy.constants as const
 
-def setup(params):
+def setup(params, filename):
     """ 
     Update the ``params`` class with QSP Potential parameters. 
     The QSP Potential is given by eq.(5) in Ref. [2]_ .
@@ -43,6 +43,16 @@ def setup(params):
     QSP_matrix[3,:,:] = e-e term factor
     QSP_matrix[4,:,:] = e-e exp factor 4pi/deBroglie/ln(2)
     """
+    # open the input file to read Yukawa parameters
+    with open(filename, 'r') as stream:
+        dics = yaml.load(stream, Loader=yaml.FullLoader)
+        for lkey in dics:
+            if (lkey == "Potential"):
+                for keyword in dics[lkey]:
+                    for key, value in keyword.items():
+                        if (key == "QSP_type"): # screening
+                            params.Potential.QSP_type = value
+
     if ( params.P3M.on):
         QSP_matrix = np.zeros( (6, params.num_species, params.num_species) )
     else:
@@ -165,7 +175,10 @@ def setup(params):
         params.force = QSP_force_PP
 
     if (params.Potential.method == "P3M"):
-        params.force = QSP_force_P3M
+        if (params.Potential.QSP_type == "Deutsch"):
+            params.force = Deutsch_force_P3M
+        elif (params.Potential.QSP_type == "Kelbg"):
+            params.force = Kelbg_force_P3M
         # P3M parameters
         params.P3M.hx = params.Lx/params.P3M.Mx
         params.P3M.hy = params.Ly/params.P3M.My
@@ -231,7 +244,7 @@ def QSP_force_PP(r, pot_matrix_ij):
     return U, force
 
 @nb.njit
-def QSP_force_P3M(r, pot_matrix):
+def Deutsch_force_P3M(r, pot_matrix):
     """ 
     Calculates the QSP Force between two particles when the P3M algorithm is chosen.
 
@@ -290,6 +303,68 @@ def QSP_force_P3M(r, pot_matrix):
 
     return U, force
 
+@nb.njit
+def Kelbg_force_P3M(r, pot_matrix):
+    """ 
+    Calculates the QSP Force between two particles when the P3M algorithm is chosen.
+
+    Parameters
+    ----------
+    r : float
+        Distance between two particles.
+
+    pot_matrix_ij : array
+        It contains potential dependent variables.
+
+    Returns
+    -------
+    U : float
+        Potential.
+                
+    force : float
+        Force between two particles.
+    
+    """
+    """
+    pot_matrix[0] = de Broglies wavelength,
+    pot_matrix[1] = qi*qj/4*pi*eps0
+    pot_matrix[2] = 2pi/deBroglie
+    pot_matrix[3] = e-e term factor
+    pot_matrix[4] = e-e exp factor 4pi/L_dB^2/ln(2)
+    """
+
+    # Ewald force corresponding to the 1/r term of the potential
+
+    A = pot_matrix[1]
+    C = pot_matrix[2] 
+    C2 = C*C
+    D = pot_matrix[3]
+    F = pot_matrix[4]
+    alpha = pot_matrix[5]   # Ewald parameter alpha
+
+    # Ewald short-range force and potential
+    a2 = alpha*alpha 
+    r2 = r*r
+    U_s_r = A*mt.erfc(alpha*r)/r
+
+    f1 = mt.erfc(alpha*r)/r2/r
+    f2 = (2.0*alpha/np.sqrt(np.pi)/r2)*np.exp(- a2*r2 )
+    fterm1 = A*( f1 + f2 )
+
+    # Exponential terms of the potential and force
+    U_pp = -A*np.exp(-C2*r2/np.pi)/r
+    U_pp2 = + A*C*mt.erfc(C*r/np.sqrt(pi))
+    ee_pot_term = D*np.exp(-F*r2)
+
+    fterm_pp = -A*(2.0*C2*r2 + np.pi)*np.exp(- C2*r2/np.pi)/(np.pi*r*r2)
+    #erfc derivative
+    fterm_pp2 = 2.0*A*C2*np.exp(- C2*r2/np.pi)/r/np.pi
+    fterm_ee = 2.0*D*F*np.exp(-F*r2)
+
+    U = U_s_r + U_pp + U_pp2 + ee_pot_term
+    force = fterm1 + fterm_pp + fterm_pp2 + fterm_ee
+
+    return U, force
 
 
 @nb.njit

src/S_pot_Yukawa.py
@@ -291,7 +291,6 @@ def setup(params, filename):
         fourpie0 = 4.0*np.pi*params.eps0
 
     twopi = 2.0*np.pi
-    hbar2 = params.hbar*params.hbar        
     beta_i = 1.0/(params.kB*params.Ti)
 
     # open the input file to read Yukawa parameters
@@ -361,9 +360,6 @@ def setup(params, filename):
         # Thomas-Fermi length obtained from compressibility. See eq.(10) in Ref. [3]_
         lambda_TF = np.sqrt((4.0*np.pi**2*e_0*h_bar**2)/(m_e*e**2)*np.sqrt(2*beta*h_bar**2/m_e)/(4.0*fdint_fdk_vec(k=-0.5, phi=eta))) 
 
-        Yukawa_matrix[0, :, :] = 1.0/lambda_TF # kappa/ai
-
-
     # Calculate the Potential Matrix
     Z53 = 0.0
     Z_avg = 0.0
@@ -395,6 +391,7 @@ def setup(params, filename):
     # Calculate the (total) plasma frequency
     if (params.Control.units == "cgs"):
         params.lambda_TF = lambda_TF*100.  # meter to centimeter
+        Yukawa_matrix[0, :, :] = 1.0/params.lambda_TF # kappa/ai
         wp_tot_sq = 0.0
         for i in range(params.num_species):
             wp2 = 4.0*np.pi*params.species[i].charge**2*params.species[i].num_density/params.species[i].mass
@@ -405,6 +402,7 @@ def setup(params, filename):
 
     elif (params.Control.units == "mks"):
         params.lambda_TF = lambda_TF
+        Yukawa_matrix[0, :, :] = 1.0/params.lambda_TF # kappa/ai
         wp_tot_sq = 0.0
         for i in range(params.num_species):
             wp2 = params.species[i].charge**2*params.species[i].num_density/(params.species[i].mass*const.epsilon_0)
@@ -429,7 +427,7 @@ def setup(params, filename):
         params.Potential.matrix[-1,:,:] = params.P3M.G_ew
         # Optimized Green's Function
         params.P3M.G_k, params.P3M.kx_v, params.P3M.ky_v, params.P3M.kz_v, params.P3M.PM_err, params.P3M.PP_err = gf_opt(params.P3M.MGrid,\
-            params.P3M.aliases, params.Lv, params.P3M.cao, params.N, params.Potential.matrix, params.Potential.rc, fourpie0)
+            params.P3M.aliases, params.Lv, params.P3M.cao, params.N, params.Potential.matrix, params.Potential.rc, params.fourpie0)
 
         # Include the charges in the Force errors. Prefactor in eq.(29) Ref.[1]_
         # Notice that the equation was derived for a single component plasma. 