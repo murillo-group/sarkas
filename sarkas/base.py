Implemented deutsch qsp. Fixed a bug in rdf calculation.

sarkas/base.py
@@ -439,7 +439,8 @@ class Parameters:
         self.box_volume = abs(np.dot(np.cross(self.e1, self.e2), self.e3))
 
         self.dimensions = np.count_nonzero(self.box_lengths)  # no. of dimensions
-
+        # Transform the list of species names into a np.array
+        self.species_names = np.array(self.species_names)
         # Redundancy!!!
         self.T_desired = self.total_ion_temperature
 
@@ -1272,6 +1273,6 @@ class Species:
             Electrostatic * Thermal constants.
 
         """
-        self.ai = (self.charge / z_avg) ** (1. / 3.) * a_ws
-
+        # self.ai = (self.charge / z_avg) ** (1. / 3.) * a_ws
+        self.ai = (3.0/(4.0 * np.pi * self.number_density))**(1./3.)
         self.coupling = self.charge ** 2 / (self.ai * const * self.temperature)

sarkas/potentials/base.py
@@ -95,6 +95,7 @@ class Potential:
         self.method = "PP"
         self.matrix = None
         self.force_error = None
+        self.measure = False
         self.box_lengths = 0.0
         self.box_volume = 0.0
         self.fourpie0 = 0.0
@@ -123,8 +124,15 @@ class Potential:
 
         self.__dict__.update(input_dict)
 
-    def setup(self, params):
+    def setup(self, params) -> None:
+        """Setup the potential class.
 
+        Parameters
+        ----------
+        params: sarkas.base.Parameters
+            Simulation's parameters.
+
+        """
         # Check for cutoff radius
         if not self.type.lower() == 'fmm':
             self.linked_list_on = True  # linked list on
@@ -134,82 +142,91 @@ class Potential:
                 self.rc = params.box_lengths.min() / 2.
                 self.linked_list_on = False  # linked list off
 
-            if self.method == "PP" and self.rc > params.box_lengths.min() / 2.:
+            if self.rc > params.box_lengths.min() / 2.:
                 print("\nWARNING: The cut-off radius is > L/2. L/2 = ", params.box_lengths.min() / 2,
                       "will be used as rc")
                 self.rc = params.box_lengths.min() / 2.
                 self.linked_list_on = False  # linked list off
 
-        if hasattr(self, "kappa") and hasattr(self, "Te"):
-            print(
-                "\nWARNING: You have provided both kappa and Te while only one is needed."
-                "kappa will be used to calculate the screening parameter.")
+        if self.type.lower() == 'qsp':
+            mask = params.species_names == 'e'
+            self.electron_temperature = params.species_temperatures[mask]
+            params.ne = float(params.species_num_dens[mask])
+            params.electron_temperature = float(params.species_temperatures[mask])
+            params.qe = float(params.species_charges[mask])
+            params.me = float(params.species_masses[mask])
+        else:
+            params.ne = params.species_charges.transpose() @ params.species_concentrations * params.total_num_density / params.qe
 
+            # Check electron properties
+            if hasattr(self, "electron_temperature_eV"):
+                self.electron_temperature = params.eV2K * self.electron_temperature_eV
+            if hasattr(self, "electron_temperature"):
+                params.electron_temperature = self.electron_temperature
+            else:
+                params.electron_temperature = params.total_ion_temperature
+                # if the electron temperature is not defined. The total ion temperature will be used for it.
         self.calc_electron_properties(params)
 
         if hasattr(self, "kappa"):
             # Thomas-Fermi Length
             params.lambda_TF = params.a_ws / self.kappa
 
+        # Update potential-specific parameters
         # Coulomb potential
         if self.type.lower() == "coulomb":
             from sarkas.potentials import coulomb
             coulomb.update_params(self, params)
-
         # Yukawa potential
         if self.type.lower() == "yukawa":
             from sarkas.potentials import yukawa
             yukawa.update_params(self, params)
-
         # exact gradient-corrected screening (EGS) potential
         if self.type.lower() == "egs":
             from sarkas.potentials import egs
             egs.update_params(self, params)
-
         # Lennard-Jones potential
         if self.type.lower() == "lj":
             from sarkas.potentials import lennardjones as lj
             lj.update_params(self, params)
-
         # Moliere potential
         if self.type.lower() == "moliere":
             from sarkas.potentials import moliere
             moliere.update_params(self, params)
-
         # QSP potential
         if self.type.lower() == "qsp":
             from sarkas.potentials import qsp
             qsp.update_params(self, params)
 
-        if self.method == 'P3M':
+        # Compute pppm parameters
+        if self.method == 'P3M' or self.method.lower() == 'pppm':
             self.pppm_on = True
             self.pppm_setup(params)
 
         # Copy needed parameters
-        self.box_lengths = params.box_lengths
+        self.box_lengths = np.copy(params.box_lengths)
         self.box_volume = params.box_volume
         self.fourpie0 = params.fourpie0
         self.QFactor = params.QFactor
         self.total_net_charge = params.total_net_charge
         self.measure = params.measure
 
-    def calc_electron_properties(self, params):
+    @staticmethod
+    def calc_electron_properties(params):
+        """Calculate electronic parameters.
+
+        Parameters
+        ----------
+        params: sarkas.base.Parameters
+            Simulation's parameters.
+
+        """
 
         twopi = 2.0 * np.pi
-        if hasattr(self, "electron_temperature_eV"):
-            self.electron_temperature = params.eV2K * self.electron_temperature_eV
-
-        if hasattr(self, "electron_temperature"):
-            params.Te = self.electron_temperature
-        else:
-            params.Te = params.total_ion_temperature
-            # if the electron temperature is not defined. The total ion temperature will be used for it.
-
-        params.ne = params.species_charges.transpose() @ params.species_concentrations * params.total_num_density / params.qe
         # Calculate electron gas properties
         fdint_fdk_vec = np.vectorize(fdint.fdk)
         fdint_ifd1h_vec = np.vectorize(fdint.ifd1h)
-        beta_e = 1. / (params.kB * params.Te)
+        beta_e = 1. / (params.kB * params.electron_temperature)
         lambda_DB = np.sqrt(twopi * params.hbar2 * beta_e / params.me)
         lambda3 = lambda_DB ** 3
         # chemical potential of electron gas/(kB T). See eq.(4) in Ref.[3]_
@@ -218,25 +235,27 @@ class Potential:
         params.lambda_TF = np.sqrt(params.fourpie0 * np.sqrt(np.pi) * lambda3 / (
                 8.0 * np.pi * params.qe ** 2 * beta_e * fdint_fdk_vec(k=-0.5, phi=params.eta_e)))
 
-        params.rs = params.a_ws / params.a0
+        params.ae_ws = (3.0/(4.0 * np.pi * params.ne))**(1./3.) # Electron WS radius
+        params.rs = params.ae_ws / params.a0
         kF = (3.0 * np.pi ** 2 * params.ne) ** (1. / 3.)
         params.fermi_energy = params.hbar2 * kF ** 2 / (2.0 * params.me)
-        params.electron_degeneracy_parameter = params.kB * params.Te / params.fermi_energy
+        params.electron_degeneracy_parameter = params.kB * params.electron_temperature / params.fermi_energy
         params.relativistic_parameter = params.hbar * kF / (params.me * params.c0)
+        # Eq. 1 in Murillo Phys Rev E 81 036403 (2010)
+        params.electron_coupling = params.qe**2/(
+                params.fourpie0 * params.fermi_energy * params.ae_ws* np.sqrt(params.electron_degeneracy_parameter**2))
+        # Warm Dense Matter Parameter, Eq.3 in Murillo Phys Rev E 81 036403 (2010)
+        params.wdm_parameter = 2.0/(params.electron_degeneracy_parameter + 1.0/params.electron_degeneracy_parameter)
+        params.wdm_parameter *= 2.0/(params.electron_coupling + 1.0/params.electron_coupling)
 
     def update_linked_list(self, ptcls):
         """
-        Calculate the Potential and update particles' accelerations.
+        Calculate the pp part of the acceleration.
 
         Parameters
         ----------
         ptcls: sarkas.base.Particles
             Particles data.
-
-        Returns
-        -------
-        U : float
-            Total Potential.
 
         """
         ptcls.potential_energy, ptcls.acc = force_pp.update(ptcls.pos, ptcls.id, ptcls.masses, self.box_lengths,
@@ -250,7 +269,15 @@ class Potential:
             ptcls.potential_energy += 2.0 * np.pi * np.sum(dipole ** 2) / (3.0 * self.box_volume * self.fourpie0)
 
     def update_brute(self, ptcls):
+        """
+        Calculate particles' acceleration and potential brutally.
 
+        Parameters
+        ----------
+        ptcls: sarkas.base.Particles
+            Particles data.
+
+        """
         ptcls.potential_energy, ptcls.acc = force_pp.update_0D(ptcls.pos, ptcls.id, ptcls.masses, self.box_lengths,
                                                self.rc, self.matrix, self.force,
                                                self.measure, ptcls.rdf_hist)
@@ -261,7 +288,14 @@ class Potential:
             ptcls.potential_energy += 2.0 * np.pi * np.sum(dipole ** 2) / (3.0 * self.box_volume * self.fourpie0)
 
     def update_pm(self, ptcls):
+        """Calculate the pm part of the potential and acceleration.
 
+        Parameters
+        ----------
+        ptcls : sarkas.base.Particles
+            Particles' data
+
+        """
         U_long, acc_l_r = force_pm.update(ptcls.pos, ptcls.charges, ptcls.masses,
                                           self.pppm_mesh, self.box_lengths, self.pppm_green_function,
                                           self.pppm_kx,
@@ -277,11 +311,26 @@ class Potential:
         ptcls.acc += acc_l_r
 
     def update_pppm(self, ptcls):
+        """Calculate particles' potential and accelerations using pppm method.
 
+        Parameters
+        ----------
+        ptcls : sarkas.base.Particles
+            Particles' data
+
+        """
         self.update_linked_list(ptcls)
         self.update_pm(ptcls)
 
-    def pppm_setup(self, params):
+    def pppm_setup(self, params) -> None:
+        """Calculate the P3M parameters.
+
+        Parameters
+        ----------
+        params : sarkas.base.Parameters
+            Simulation's parameters
+
+        """
         # P3M parameters
         self.pppm_h_array = params.box_lengths / self.pppm_mesh
         if not isinstance(self.pppm_mesh, np.ndarray):

sarkas/potentials/egs.py
@@ -28,7 +28,7 @@ def update_params(potential, params):
 
     fdint_fdk_vec = np.vectorize(fdint.fdk)
     fdint_dfdk_vec = np.vectorize(fdint.dfdk)
-    beta_e = 1. / (params.kB * params.Te)
+    beta_e = 1. / (params.kB * params.electron_temperature)
     thermal_wavelength = np.sqrt(twopi * params.hbar2 * beta_e / params.me)
     # eq. (14) of Ref. [1]_
     # params.nu = np.sqrt(8 * (4.0 * np.pi * params.qe ** 2 * beta_e / params.fourpie0 / thermal_wavelength))

sarkas/potentials/qsp.py
@@ -31,6 +31,9 @@ def update_params(potential, params):
 
     Parameters
     ----------
+    potential : sarkas.potential.Potential
+        Class handling potential form.
+
     params : object
         Simulation's parameters
 
@@ -54,100 +57,70 @@ def update_params(potential, params):
     # Default attributes
     if not hasattr(potential, 'qsp_type'):
         potential.qsp_type = 'Deutsch'
-
     if not hasattr(potential, 'qsp_pauli'):
         potential.qsp_pauli = True
 
     two_pi = 2.0 * np.pi
     four_pi = 2.0 * two_pi
-    params.ne = params.species[0].num_density
-    params.ae = (3.0 / (four_pi * params.ne)) ** (1.0 / 3.0)  # e WS
-    params.rs = params.ae / params.a0  # e coupling parameter
-    params.Te = params.species[0].temperature
+    log2 = np.log(2.0)
 
     # Redefine ion temperatures and ion total number density
-    params.total_ion_temperature = 0.
-    params.ni = 0.
-    for isp in range(1, params.num_species):
-        params.total_ion_temperature += params.species[isp].concentration * params.species[isp].temperature
-        params.ni += params.species[isp].num_density
+    mask = params.species_names != 'e'
+    params.total_ion_temperature = params.species_concentrations[mask].transpose() * params.species_temperatures[mask]
+    params.ni = np.sum(params.species_num_dens[mask])
 
     # Calculate the total and ion Wigner-Seitz Radius from the total density
-    params.a_ws = (3.0 / (four_pi * params.total_num_density)) ** (1. / 3.)
     params.ai = (3.0 / (four_pi * params.ni)) ** (1.0 / 3.0)  # Ion WS
 
-    beta_e = 1.0 / (params.kB * params.Te)
-    beta_i = 1.0 / (params.kB * params.total_ion_temperature)
+    deBroglie_const = two_pi * params.hbar2 / params.kB
 
     QSP_matrix = np.zeros((5, params.num_species, params.num_species))
-    for i, sp1 in enumerate(params.species):
-        m1 = sp1.mass
-        q1 = sp1.charge
+    for i, name1 in enumerate(params.species_names):
+        m1 = params.species_masses[i]
+        q1 = params.species_charges[i]
 
-        for j, sp2 in enumerate(params.species):
-            m2 = sp2.mass
-            q2 = sp2.charge
+        for j, name2 in enumerate(params.species_names):
+            m2 = params.species_masses[j]
+            q2 = params.species_charges[j]
+
             reduced = (m1 * m2) / (m1 + m2)
-            if i == 0:
-                Lambda_dB = np.sqrt(two_pi * beta_e * params.hbar2 / reduced)
-                if j == i:  # e-e
-                    QSP_matrix[2, i, j] = np.log(2.0) / beta_e
-                    QSP_matrix[3, i, j] = four_pi / (np.log(2.0) * Lambda_dB ** 2)
+
+            if name1 == 'e' or name2 == 'e':
+                # Use electron temperature in e-e and e-i interactions
+                Lambda_dB = np.sqrt(deBroglie_const / (reduced * params.electron_temperature))
             else:
-                Lambda_dB = np.sqrt(two_pi * beta_i * params.hbar2 / reduced)
+                # Use ion temperature in i-i interactions only
+                Lambda_dB = np.sqrt(deBroglie_const / (reduced * params.total_ion_temperature))
+
+            if name2 == name1:  # e-e
+                QSP_matrix[2, i, j] = log2 * params.kB * params.electron_temperature
+                QSP_matrix[3, i, j] = four_pi / (log2 * Lambda_dB ** 2)
 
             QSP_matrix[0, i, j] = q1 * q2 / params.fourpie0
             QSP_matrix[1, i, j] = two_pi / Lambda_dB
 
-    params.QFactor /= params.fourpie0
-    if not params.potential.QSP_Pauli:
+    if not potential.qsp_pauli:
         QSP_matrix[2, :, :] = 0.0
 
-    params.potential.matrix = QSP_matrix
-    params.potential.Gamma_eff = abs(params.potential.matrix[0, 0, 1]) / (params.ai * params.kB * params.total_ion_temperature)
+    QSP_matrix[4, :, :] = potential.pppm_alpha_ewald
+    potential.matrix = QSP_matrix
 
-    # Calculate the (total) plasma frequency
-    wp_tot_sq = 0.0
-    for i, sp in enumerate(params.species):
-        wp2 = four_pi * sp.charge ** 2 * sp.number_density / (sp.mass * params.fourpie0)
-        sp.wp = np.sqrt(wp2)
-        wp_tot_sq += wp2
-
-    params.total_plasma_frequency = np.sqrt(wp_tot_sq)
-
-    if params.potential.QSP_type == "Deutsch":
-        params.force = Deutsch_force_P3M
+    if potential.qsp_type.lower() == "deutsch":
+        potential.force = deutsch_force
         # Calculate the PP Force error from the e-e diffraction term only.
-        params.pppm.PP_err = np.sqrt(two_pi * params.potential.matrix[1, 0, 0])
-        params.pppm.PP_err *= np.exp(- params.potential.rc * params.potential.matrix[1, 0, 0])
+        params.pppm_pp_err = np.sqrt(two_pi * potential.matrix[1, 0, 0])
+        params.pppm_pp_err *= np.exp(- potential.rc * potential.matrix[1, 0, 0])
+        params.pppm_pp_err *= params.a_ws ** 2 * np.sqrt(params.total_num_ptcls / params.box_volume)
 
-    elif params.potential.QSP_type == "Kelbg":
-        params.force = Kelbg_force_P3M
+    elif potential.qsp_type.lower() == "kelbg":
+        potential.force = kelbg_force
         # TODO: Calculate the PP Force error from the e-e diffraction term only.
-        params.pppm.PP_err = np.sqrt(two_pi * params.potential.matrix[1, 0, 0])
-        params.pppm.PP_err *= np.exp(- params.potential.rc * params.potential.matrix[1, 0, 0])
-
-    # P3M parameters
-    params.pppm.hx = params.Lx / params.pppm.Mx
-    params.pppm.hy = params.Ly / params.pppm.My
-    params.pppm.hz = params.Lz / params.pppm.Mz
-    params.potential.matrix[4, :, :] = params.pppm.G_ew
-    # Calculate the Optimized Green's Function
-    constants = np.array([0.0, params.pppm.G_ew, params.fourpie0])
-    params.pppm.G_k, params.pppm.kx_v, params.pppm.ky_v, params.pppm.kz_v, params.pppm.PM_err = gf_opt(
-        params.pppm.MGrid, params.pppm.aliases, params.box_lengths, params.pppm.cao, constants)
-
-    # Complete PM and PP Force error calculation
-    params.pppm.PM_err *= np.sqrt(params.total_num_ptcls) * params.a_ws ** 2 * params.fourpie0 / params.box_volume ** (2. / 3.)
-    params.pppm.PP_err *= params.a_ws ** 2 * np.sqrt(params.total_num_ptcls / params.box_volume)
-    # Calculate the total force error
-    params.pppm.F_err = np.sqrt(params.pppm.PM_err ** 2 + params.pppm.PP_err ** 2)
-
-    return
+        params.pppm_pp_err = np.sqrt(two_pi * potential.matrix[1, 0, 0])
+        params.pppm_pp_err *= np.exp(- potential.rc * potential.matrix[1, 0, 0])
 
 
 @njit
-def Deutsch_force_P3M(r, pot_matrix):
+def deutsch_force(r, pot_matrix):
     """ 
     Calculate Deutsch QSP Force between two particles.
 
@@ -185,17 +158,17 @@ def Deutsch_force_P3M(r, pot_matrix):
 
     # Ewald short-range potential and force terms
     U_ewald = A * mt.erfc(alpha * r) / r
-    f_ewald = U_ewald / r2  # 1/r derivative
-    f_ewald += A * (2.0 * alpha / np.sqrt(np.pi) / r2) * np.exp(- a2 * r2)  # erfc derivative
+    f_ewald = U_ewald / r  # 1/r derivative
+    f_ewald += A * (2.0 * alpha / np.sqrt(np.pi) ) * np.exp(- a2 * r2)/r  # erfc derivative
 
     # Diffraction potential and force term
     U_diff = -A * np.exp(-C * r) / r
-    f_diff = U_diff / r2  # 1/r derivative
-    f_diff += - A * C * np.exp(-C * r) / r2  # exp derivative
+    f_diff = U_diff / r  # 1/r derivative
+    f_diff += -A * C * np.exp(-C * r) / r  # exp derivative
 
     # Pauli potential and force terms
     U_pauli = D * np.exp(-F * r2)
-    f_pauli = 2.0 * D * F * np.exp(-F * r2)
+    f_pauli = 2.0 * r * D * F * np.exp(-F * r2)
 
     U = U_ewald + U_diff + U_pauli
     force = f_ewald + f_diff + f_pauli
@@ -204,7 +177,7 @@ def Deutsch_force_P3M(r, pot_matrix):
 
 
 @njit
-def Kelbg_force_P3M(r, pot_matrix):
+def kelbg_force(r, pot_matrix):
     """ 
     Calculates the QSP Force between two particles when the P3M algorithm is chosen.
 